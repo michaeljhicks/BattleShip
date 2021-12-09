2daf2c943ca8a876245bafdd331fdfb48e6cafe1
glsorensen <gunnar.l.sorensen@gmail.com>
2021-12-08 20:26:34 -0700

    Test: wrote tests through fired_upon? in cell_spec.rb to develop the cell class from (class.rb)

---
+++ spec/cell_spec.rb
@@ -0,0 +1,56 @@
+require './lib/ship'
+require './lib/cell'
+
+RSpec.describe Cell do
+  context 'Iteration I Cell' do
+
+    let(:cell) {Cell.new("B4")}
+
+    describe 'Iteration I' do
+      it 'exists' do
+
+        expect(cell).to be_a(Cell)
+      end
+      it 'has cordinates' do
+
+        expect(cell.coordinate).to eq("B4")
+      end
+
+      it 'has a ship' do
+
+        expect(cell.ship).to eq(nil)
+      end
+
+      it 'is an empty cell' do
+
+      expect(cell.empty?).to eq(true)
+      end
+
+      it 'can have a ship' do
+        cruiser = Ship.new("Cruiser", 3)
+
+        cell.place_ship(cruiser)
+        expect(cell.empty?).to eq(false)
+        expect(cell.ship).to eq(cruiser)
+      end
+
+    describe '#fire_upon and #fired_upon?' do
+        it 'defaults non-fired upon...' do
+          cell = Cell.new("B4")
+          expect(cell.fired_upon?).to eq(false)
+        end
+
+        it 'can be fired upon and lower ship health' do
+          cell = Cell.new("B4")
+          cruiser = Ship.new("Cruiser", 3)
+
+          cell.place_ship(cruiser)
+
+          cell.fire_upon
+          expect(cell.fired_upon?).to be(true)
+          expect(cell.ship.health).to be(2)
+        end
+      end
+    end
+  end
+end 
