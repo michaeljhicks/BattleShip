2863917268710ec0e52d05dc0ba44858f230d55e
Michael Hicks <hicks.michael04@gmail.com>
2021-12-15 15:59:39 -0700

    Refactor player.rb and player_spec.rb

--- spec/player_spec.rb
+++ spec/player_spec.rb
@@ -3,27 +3,28 @@ require './lib/cell'
 require './lib/board'
 require './lib/player'
 
-
-
 RSpec.describe Player do
+
   context 'Iteration III' do
 
     let(:board) {Board.new}
     let(:player) {Player.new(board)}
 
     describe 'Player' do
-      it 'exists' do
 
+      it 'exists' do
         expect(player).to be_a(Player)
       end
+
     end
 
     describe 'has ships' do
-      it 'exists' do
 
+      it 'exists' do
         expect(player.cruiser).to be_a(Ship)
         expect(player.submarine).to be_a(Ship)
       end
+      
     end
   end
 end
