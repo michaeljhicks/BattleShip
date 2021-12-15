90202a43ba9c854e8661d58b4e193f28034a6832
Michael Hicks <hicks.michael04@gmail.com>
2021-12-15 15:32:45 -0700

    feat: separated both player and computer firing methods and place them into Turn class

--- lib/battleship_gameplay.rb
+++ lib/battleship_gameplay.rb
@@ -8,43 +8,88 @@ require './lib/computer'
 class Battleship
   attr_reader :greeting, :game_play
 
-  def initialize
-    @player_board = Board.new
-    @comp_board = Board.new
-     @computer = Computer.new(@comp_board)
-     @player = Player.new(@player_board)
-    # @cruiser = Ship.new('cruiser', 3)
-    # @submarine = Ship.new('submarine', 2)
-
-  end
-
-
-  def greeting
-    puts "Welcome to BATTLESHIP\n" +
-      "Enter p to play. Enter q to quit."
-      answer = gets.chomp
-    if answer == 'p'
-      puts "Let's play!"
-      game_play
-    else answer == 'q'
-      puts "Okay bye!"
+    def initialize
+      # @turn = Turn.new
+      @player_board = Board.new
+      @comp_board = Board.new
+      @computer = Computer.new(@comp_board)
+      @player = Player.new(@player_board)
+      @cruiser = Ship.new('cruiser', 3)
+      @submarine = Ship.new('submarine', 2)
+      # ideally, we wanted to initialize @turn = Turn.new which removes the need to require above lines 13 - 18
     end
-  end
 
-  def game_play #set up boards, player Computer
-    @computer.place_cruiser # computer sets up board
-    @computer.place_submarine
-    @player.ship_placement
-    show_boards # player sets up board
-  end
 
-  def show_boards
-    p "**************COMPUTER BOARD************"
-    puts @computer.comp_board.render
-    p "***************PLAYER BOARD**************"
-    puts @player.player_board.render(true)
-    puts "Take your shot! Enter a coordinate you wish to \n" +
+    def greeting
+
+      # puts ```                         __________         __    __  .__           _________.__    .__
+      #     #   /\|\/\     /\|\/\     /\|\/\   \______   \_____ _/  |__/  |_|  |   ____  /   _____/|  |__ |__|_____    /\|\/\     /\|\/\     /\|\/\
+      #     #  _)    (__  _)    (__  _)    (__  |    |  _/\__  \\   __\   __\  | _/ __ \ \_____  \ |  |  \|  \____ \  _)    (__  _)    (__  _)    (__
+      #     #  \_     _/  \_     _/  \_     _/  |    |   \ / __ \|  |  |  | |  |_\  ___/ /        \|   Y  \  |  |_> > \_     _/  \_     _/  \_     _/
+      #     #    )    \     )    \     )    \   |______  /(____  /__|  |__| |____/\___  >_______  /|___|  /__|   __/    )    \     )    \     )    \
+      #     #    \/\|\/     \/\|\/     \/\|\/          \/      \/                     \/        \/      \/   |__|       \/\|\/     \/\|\/     \/\|\/   ```
+      #
+
+      puts "Welcome to BATTLESHIP\n" +
+        "Enter p to play. Enter q to quit."
+        answer = gets.chomp
+      if answer == 'p'
+        puts "Let's play!"
+        game_play
+      else answer == 'q'
+        puts "Okay bye!"
+      end
+    end
+
+    def game_play #set up boards, player Computer
+      @computer.place_cruiser # computer sets up board
+      @computer.place_submarine
+      @player.ship_placement
+      show_boards # show_boards # player sets up board
+      # loop until game over
+        # calls player shot method
+        # calls player takes shot method
+        # calls computer shot method
+          # insert @win_criteria method (from below)
+          # this is all looped until game over.....break
+      # if break from loops occurs, yields return message "congratulations, you've won" ...OR...
+        # "Our sincerest condolences; the computer has won"
+    end
+
+    def show_boards
+      p "=============COMPUTER BOARD============="
+      puts @computer.comp_board.render      
+      p "==============PLAYER BOARD=============="
+      puts @player.player_board.render(true)
+      puts
+      puts "Take your shot! Enter a coordinate you wish to \n" +
       "fire upon!"
+
+    end
+
+    def win_criteria
+      # if a players cruiser and sub are sunk = loss
+      # if both computers ships are sunk = win
+
+    end
+
+
+
+
+
+
+
+
+end
+
+
+
+
+
+
+
+
+
 
       # computer takes first turn
         # turn confirmed: H, M, S
@@ -58,8 +103,7 @@ class Battleship
 
     #ask computer and person if ships are sunk, while loop
     #how to store turn state
-  end
-end
+
 
 
 #
@@ -77,12 +121,6 @@ end
 #
 #   def game_menu
 #
-#     # puts ```                         __________         __    __  .__           _________.__    .__
-#     #   /\|\/\     /\|\/\     /\|\/\   \______   \_____ _/  |__/  |_|  |   ____  /   _____/|  |__ |__|_____    /\|\/\     /\|\/\     /\|\/\
-#     #  _)    (__  _)    (__  _)    (__  |    |  _/\__  \\   __\   __\  | _/ __ \ \_____  \ |  |  \|  \____ \  _)    (__  _)    (__  _)    (__
-#     #  \_     _/  \_     _/  \_     _/  |    |   \ / __ \|  |  |  | |  |_\  ___/ /        \|   Y  \  |  |_> > \_     _/  \_     _/  \_     _/
-#     #    )    \     )    \     )    \   |______  /(____  /__|  |__| |____/\___  >_______  /|___|  /__|   __/    )    \     )    \     )    \
-#     #    \/\|\/     \/\|\/     \/\|\/          \/      \/                     \/        \/      \/   |__|       \/\|\/     \/\|\/     \/\|\/   ```
 #
 #
 #
--- lib/turn.rb
+++ lib/turn.rb
@@ -25,5 +25,44 @@ class Turn
     "fire upon!"
 
   end
+
+  def player_shot #here, we have everything to render
+    loop do
+      player_choice = gets.chomp.upcase
+      if @computer.comp_board.valid_coordinate(player_choice) == true
+        if @computer.comp_board.cells(player_choice).fired_upon? == true
+          puts "You've already taken this shot. Please try again."
+          #return back to player choice to retrieve a new/accurate selection
+          # if coordinate is valid, take the shot
+            # capture + redirect, and account for invalid coordinates entered
+        end
+      end
+    end
+
+  def player_takes_shot(player_choice) # where we're determining if player has sunk and/or hit ship
+      # reference the fire_upon? method in Cell Class
+        #if cell.empty? == false
+          # puts "hit on ship"
+        # if shot hits the cell + sunk? method == true.....then "you sunk their ship!"
+          #else puts "missed shot!"
+  end
+
+  def computer_shot
+    loop do
+      computer_choice = @computer.merge_select
+        if @player.player_board.cells(computer_choice).fired_upon? == true
+          # return back to computer for its next choice
+          if cell.empty? == false
+            # computer scored a 'hit'
+            # if shot hits the cell + sunk? method == true.....then "computer sunk your ship!"
+            if cell.empty? == true
+              # computer missed
+          #return back to player choice to retrieve a new/accurate selection
+          # if coordinate is valid, take the shot
+            # capture + redirect, and account for invalid coordinates entered
+        end
+      end
+    end
+  end
   # prompt player shot coordinates
   # report player shot result
