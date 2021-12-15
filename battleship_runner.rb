4f5d4a9b9c5c71f13907d3dec023b4ce555b220d
glsorensen <gunnar.l.sorensen@gmail.com>
2021-12-14 19:15:54 -0700

    refactor: removed non working code from turn

--- battleship_runner.rb
+++ battleship_runner.rb
@@ -1,19 +1,9 @@
 require './lib/battleship_gameplay'
 require './lib/board'
-require './lib/cell'
-require './lib/computer'
-require './lib/player'
 require './lib/ship'
+require './lib/cell'
+require './lib/player'
+require './lib/computer'
 require './lib/turn'
-
-def begin
-  loop do
-    game = BattleShip.new
-  end
-end
-
-
-# computer = Computer.new
-# person = Player.new
-# battleship = BattleShip.new(computer, person)
-# battleship.start
+battleship = Battleship.new
+battleship.greeting
--- lib/turn.rb
+++ lib/turn.rb
@@ -1,17 +1,16 @@
 require 'pry'
-require './lib/board'
 
 class Turn
-  attr_reader :computer,
-              :player_selection,
-              :comp_sunk_ships,
-              :player_sunk_ships
+  attr_reader :player_board,
+              :comp_board,
+              :computer,
+              :player
 
-  def initialize(computer, player_selection)
-    @computer = computer #already handled on both Computer + Player classes
-    @player_selection = player_selection
-    @comp_sunk_ships = 0
-    @player_sunk_ships = 0
+  def initialize
+    @player_board = Board.new
+    @comp_board = Board.new
+    @computer = Computer.new(@computer_board)
+    @player = Player.new(@player_board)
   end
 
   # show the boards
@@ -19,52 +18,10 @@ class Turn
     p "=============COMPUTER BOARD============="
     p @computer.board.render
     p "==============PLAYER BOARD=============="
-    p @player_selection.board.render(true)
-  end
+    p @player.board.render(true)
+    puts "Take your shot! Enter a coordinate you wish to \n" +
+    "fire upon!"
 
+  end
   # prompt player shot coordinates
   # report player shot result
-  def player_shot(player_choice)
-    loop do
-      if player_choice != []
-        if computer.board.valid_coordinate?(player_choice)
-          if computer.board.cells[player_choice].fired_upon? == false
-            @computer.board.cells[player_choice].fire_upon
-            if computer.board.cells[player_choice].render(true) == "M"
-              puts "Your shot on #{player_choice} was a miss."
-            elsif computer.board.cells[player_choice].render(true) == "H"
-              puts "Your shot on #{player_choice} was a hit."
-            elsif computer.board.cells[player_choice].render(true) == "X"
-              puts "Your shot on #{player_choice} sunk a ship."
-              @comp_ships_sunk += 1
-            end
-            break
-          end
-        elsif computer.board.valid_coordinate?(player_choice) == false
-          puts "Invalid coordinate! Try again:"
-          break
-        end
-      end
-    end
-  end
-
-  # prompt computer shot coordinates
-  # report computer shot result
-
-  def comp_shot
-      computer_choice = player_selection.board.cells.keys.sample
-      until player_selection.board.valid_coordinate?(computer_choice) && player_selection.board.cells[computer_choice].fired_upon? == false
-        computer_choice = player_selection.board.cells.keys.sample
-      end
-      @player_selection.board.cells[computer_choice].fire_upon
-      if player_selection.board.cells[computer_choice].render(true) == "M"
-        puts "Computer's fire on #{computer_choice} was a miss."
-      elsif player_selection.board.cells[computer_choice].render(true) == "H"
-        puts "Computer's fire on #{computer_choice} was a hit."
-      elsif player_selection.board.cells[computer_choice].render(true) == "X"
-        puts "Computer's fire on #{computer_choice} sunk a ship."
-        @player_sunk_ships += 1
-      end
-    end
-
-end
