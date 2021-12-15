be75be85611dcf3c63f2ab065e410a43a9db19c7
Michael Hicks <hicks.michael04@gmail.com>
2021-12-15 15:41:42 -0700

    Refactor ship.rb and ship_spec.rb files

--- spec/ship_spec.rb
+++ spec/ship_spec.rb
@@ -6,33 +6,28 @@ RSpec.describe Ship do
     let(:cruiser) {Ship.new("Cruiser", 3)}
 
     describe 'Iteration I' do
-      it 'exist' do
 
+      it 'exist' do
         expect(cruiser).to be_a(Ship)
       end
 
       it 'has a name' do
-
         expect(cruiser.name).to eq("Cruiser")
       end
 
       it 'has a length' do
-
         expect(cruiser.length).to eq(3)
       end
 
-      it 'has health' do 
-
+      it 'has health' do
         expect(cruiser.health).to eq(3)
       end
 
       it 'is sunk?' do
-
         expect(cruiser.sunk?).to eq(false)
       end
 
       it 'losses health' do
-
         cruiser.hit
         expect(cruiser.health).to eq(2)
         cruiser.hit
